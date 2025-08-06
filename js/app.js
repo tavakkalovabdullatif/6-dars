dc1c5059e3d2581f06599a641e734a7bc9f1928b
abdullatif <abdullatif>
2025-08-06 21:39:03 +0500

    fre

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//--------------------------------------------------1-masala--------------------------------------------------------------------------------
+
+
+function getUserInfo(user) {
+    const user = {
+    name: "Ali",
+    age: 25,
+}
+    console.log(`"Foydalanuvchi ismi ${user.name}, yoshi: ${user.age}"`)
+}
+
+getUserInfo();
